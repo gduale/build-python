2e18999d8b9802ba5642c762ebf328b7263c9673
Guillaume <guillaume.duale@gmail.com>
2018-09-16 21:56:38 +0200

    first commit

---
+++ make_python.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+## Installation de Python3 sur Debian
+
+log(){
+        m=$@
+        echo "$(date) - $m"
+}
+set -e
+V=3.7.0
+B=$(pwd)/python-${V}
+log Cleaning
+rm -rf Python-${V}
+rm -rf ${B}
+
+log Dependencies
+#Si jamais vous êtes sous CentOS ;)
+#sudo yum -y install readline-devel ncurses-devel openssl-devel sqlite-devel gdbm-devel glibc-devel wget gcc make
+sudo apt install -y wget build-essential checkinstall
+sudo apt install -y libdb-dev libreadline-gplv2-dev libncursesw5-dev libssl-dev libsqlite3-dev tk-dev libgdbm-dev libc6-dev libbz2-dev
+sudo apt-get -qqy build-dep python3
+
+log Fetching
+/usr/bin/wget https://www.python.org/ftp/python/$V/Python-$V.tgz
+tar zxvf Python-$V.tgz
+
+log Compiling
+cd Python-${V}
+./configure --prefix=$B
+make -j6
+log Install
+make altinstall
+
+cd ../
+rm -rf Python-${V}
+echo "Python est disponible ici : $B"
+
+#Création du virtualenv
+PYVENV=~/py$V
+$B/bin/python3.7 -m venv $PYVENV
+echo "Le virtual env est dispo ici : $PYVENV"
