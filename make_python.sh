0bad2f6748665360b632712de732d22cc43b90ab
Guillaume DUALE <guillaume.duale@virtual-expo.com>
2020-12-13 17:26:20 +0100

    nouvelle version, nouveaux chemins.

--- make_python.sh
+++ make_python.sh
@@ -6,7 +6,8 @@ log(){
         echo "$(date) - $m"
 }
 set -e
-V=3.7.0
+VMAJ=3.9
+V=3.9.1
 B=$(pwd)/python-${V}
 log Cleaning
 rm -rf Python-${V}
@@ -15,7 +16,7 @@ rm -rf ${B}
 log Dependencies
 #Si jamais vous êtes sous CentOS ;)
 #sudo yum -y install readline-devel ncurses-devel openssl-devel sqlite-devel gdbm-devel glibc-devel wget gcc make
-sudo apt install -y wget build-essential checkinstall python-dev
+sudo apt install -y wget build-essential python-dev
 sudo apt install -y libdb-dev libreadline-gplv2-dev libncursesw5-dev libssl-dev libsqlite3-dev tk-dev libgdbm-dev libc6-dev libbz2-dev
 sudo apt-get -qqy build-dep python3
 
@@ -25,23 +26,25 @@ sudo apt install zlib1g-dev libsqlite3-dev tk-dev -y
 sudo apt install libssl-dev openssl -y
 sudo apt install libffi-dev -y
 
-
 log Fetching
+mkdir -p ~/python
+cd ~/python
 /usr/bin/wget https://www.python.org/ftp/python/$V/Python-$V.tgz
-tar zxvf Python-$V.tgz
+tar zxf Python-$V.tgz
 
 log Compiling
 cd Python-${V}
 ./configure --prefix=$B
-make -j6
+make -j4
 log Install
 make altinstall
 
 cd ../
 rm -rf Python-${V}
-echo "Python est disponible ici : $B"
+rm -f Python-$V.tgz
+echo -e "\n\nPython est disponible ici : $B"
 
 #Création du virtualenv
-PYVENV=~/py$V
-$B/bin/python3.7 -m venv $PYVENV
+PYVENV=~/venv-python$V
+$B/bin/python${VMAJ} -m venv $PYVENV
 echo "Le virtual env est dispo ici : $PYVENV"
