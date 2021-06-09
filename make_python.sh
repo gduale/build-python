e836f64201b51e71aa19d2ab92fdd3d885511555
Guillaume DUALE <guillaume.duale@virtual-expo.com>
2021-06-09 14:37:09 +0200

    nouvelle version

--- make_python.sh
+++ make_python.sh
@@ -1,42 +1,31 @@
 #!/bin/bash
 ## Installation de Python3 sur Debian
-
-log(){
-        m=$@
-        echo "$(date) - $m"
-}
 set -e
-VMAJ=3.9
+
+mkdir -p ~/python
+cd ~/python
+
 V=3.9.1
-B=$(pwd)/python-${V}
-log Cleaning
-rm -rf Python-${V}
+VMAJ=$(echo $V|sed 's/..$//')
+B=~/python/python-${V}
 rm -rf ${B}
 
-log Dependencies
-#Si jamais vous êtes sous CentOS ;)
+#Si vous êtes sous CentOS: 
 #sudo yum -y install readline-devel ncurses-devel openssl-devel sqlite-devel gdbm-devel glibc-devel wget gcc make
 sudo apt install -y wget build-essential python-dev
 sudo apt install -y libdb-dev libreadline-gplv2-dev libncursesw5-dev libssl-dev libsqlite3-dev tk-dev libgdbm-dev libc6-dev libbz2-dev
-sudo apt-get -qqy build-dep python3
+#sudo apt-get -qqy build-dep python3
+sudo apt-get -qqy python3
 
 sudo apt install python-setuptools python-pip python-smbus -y
-sudo apt install libncursesw5-dev libgdbm-dev libc6-dev -y 
-sudo apt install zlib1g-dev libsqlite3-dev tk-dev -y
-sudo apt install libssl-dev openssl -y
-sudo apt install libffi-dev -y
+sudo apt install libncursesw5-dev libgdbm-dev libc6-dev zlib1g-dev libsqlite3-dev tk-dev libssl-dev openssl libffi-dev -y
 
-log Fetching
-mkdir -p ~/python
-cd ~/python
 /usr/bin/wget https://www.python.org/ftp/python/$V/Python-$V.tgz
 tar zxf Python-$V.tgz
 
-log Compiling
 cd Python-${V}
 ./configure --prefix=$B
 make -j4
-log Install
 make altinstall
 
 cd ../
@@ -47,4 +36,4 @@ echo -e "\n\nPython est disponible ici : $B"
 #Création du virtualenv
 PYVENV=~/venv-python$V
 $B/bin/python${VMAJ} -m venv $PYVENV
-echo "Le virtual env est dispo ici : $PYVENV"
+echo "Le virtual env est disponible ici : $PYVENV"
